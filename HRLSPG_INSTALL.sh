45cb95015b9cfbe48e0317c47c440dd77fc68d74
alex-fricker <86256077+alex-fricker@users.noreply.github.com>
2023-03-01 17:26:39 -0500

    Update HRLSPG_INSTALL.sh

--- HRLSPG_INSTALL.sh
+++ HRLSPG_INSTALL.sh
@@ -1,4 +1,4 @@
-LINK_TO_HRLSPG_REPO = h"git@github.com:alex-fricker/Hyper-Reduced-LSPG.git"
+LINK_TO_HRLSPG_REPO = "git@github.com:alex-fricker/Hyper-Reduced-LSPG.git"
 
 git clone LINK_TO_HRLSPG_REPO
 
