e3d6502c1e043bc0525988538505b9168c3ebb5d
Alex Fricker <adfricker@gmail.com>
2023-03-01 20:34:02 -0500

    Fixed install script and gitignore and CMakeLists to build with eigen library

--- HRLSPG_INSTALL.sh
+++ HRLSPG_INSTALL.sh
@@ -1,9 +1,15 @@
-LINK_TO_HRLSPG_REPO = "git@github.com:alex-fricker/Hyper-Reduced-LSPG.git"
+LINK_TO_HRLSPG_REPO = "https://github.com/alex-fricker/Hyper-Reduced-LSPG.git"
+echo "export HRLSPG_INSTALL_DIR=$HOME/Codes" >> ~/.bashrc
+source ~/.bashrc
+
+cd ${HRLSPG_INSTALL_DIR} && mkdir Libraries && cd Libraries
+
+git clone https://gitlab.com/libeigen/eigen.git
+cd eigen && mkdir build && cd build
+cmake ..
+sudo make install
 
 git clone LINK_TO_HRLSPG_REPO
-
-mkdir Build
-cd Build
-
-cmake ../
+mkdir build && cd build
+cmake ..
 make
